256d199b112e229ed34bc9fd0c26ed6959d4499d
Marcio-Souza <wache2k@gmail.com>
2020-06-01 14:18:26 -0300

    modulos de teste de envio de spam

---
+++ libpythonpro/tests/test_usuarios.py
@@ -0,0 +1,15 @@
+from libpythonpro.libpythonpro.spam.modelos import Usuario
+
+
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='Marcio')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+def test_listar_usuario(sessao):
+    usuarios = [Usuario(nome='Marcio'), Usuario(nome='Angela')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
+
